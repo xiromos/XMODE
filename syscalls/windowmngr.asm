8d7770199aa3cbe97fcffe68c52201e9644d0383
Technodon <subwayheimerwender@icloud.com>
2026-05-27 20:25:13 +0300

    saves frame buffer

--- syscalls/windowmngr.asm
+++ syscalls/windowmngr.asm
@@ -1,11 +1,13 @@
 ;=============================================================
 ;INT 0x34
 ;Syscall for drawing and deleting a window
-;AH = 0x1: draw a window                        ;input: ESI = X, EDI = Y, ECX = width, EDX = height, EBX = background color, output: EAX = window ID, EBX = CurX, ECX = CurY, EBP = pointer to title
-
+;AH = 0x1: draw a window                        ;input: ESI = X, EDI = Y, ECX = width, EDX = height, EBX = background color, EBP = pointer to title  output: EAX = window ID, EBX = CurX, ECX = CurY
+;AH = 0x2: remove window                        ;input: EDI = window_packet, bx = window id
+;AH = 0xb: draw a window but let window manager decide the size         :output: AX = window id, EBX = width, ECX = height, EDI = CurX, ESI = CurY
 bits 32
 
 window_functions:
+    cli
     cmp ah, 0x01
     je .draw_window
     cmp ah, 0x02
@@ -15,13 +17,14 @@ window_functions:
 .draw_window:
     pusha
     pusha
-    xor cx, cx
+    mov cx, 1
     mov edi, windows_list
 .search_free:
     mov al, [edi]
     cmp al, 0
     je .free_window
     inc cx
+    inc edi
     cmp cx, [num_windows]
     jb .search_free
     popa
@@ -29,9 +32,9 @@ window_functions:
     or dword [esp+8], 1
     iret
 .free_window:
+    mov byte [edi], 1
     mov [window_id], cx
     popa
-    push ebp
 
     mov [win_x], esi
     mov [win_y], edi
@@ -39,6 +42,49 @@ window_functions:
     mov [win_height], edx
     mov [win_color], ebx
 
+    movzx eax, word [window_id]
+    mov edi, window_buffer
+    imul eax, edi
+    mov edi, eax
+    ;add X
+    mov esi, [frame_buffer]
+    mov ecx, [win_x]
+    sub ecx, 1
+    movzx eax, byte [bpp]
+    mul ecx
+    add esi, eax
+
+    ;add Y
+    mov ecx, [win_y]
+    sub ecx, 20
+    mov eax, [pitch]
+    mul ecx
+    add esi, eax
+
+    mov ecx, [win_width]
+    add ecx, 2
+    movzx eax, byte [bpp]
+    mul ecx
+    mov ecx, eax
+
+    mov ebx, [win_height]
+    add ebx, 21         ;header + last row
+.loop1:
+    rep movsb
+
+    add esi, [pitch]
+    movzx ecx, byte [bpp]
+    mov eax, [win_width]
+    add eax, 2
+    mul ecx
+    sub esi, eax
+
+    mov ecx, eax
+    dec ebx
+    jnz .loop1
+;====draw the window====
+.loop3:
+    push ebp
     mov edi, [frame_buffer]
     xor edx, edx
     mov esi, [win_x]
@@ -58,11 +104,11 @@ window_functions:
     movzx eax, byte [bpp]
     mov ecx, [win_width]
     add ecx, 2
-.loop1:
+.loop4:
     mov [edi], edx
     add edi, eax
     dec ecx
-    jnz .loop1
+    jnz .loop4
 
     xor edx, edx
     add edi, [pitch]
@@ -75,7 +121,7 @@ window_functions:
     add ecx, 2
     mov edx, [win_border_color]
     dec ebx
-    jnz .loop1
+    jnz .loop4
 
     ;title the window
     pop esi
@@ -129,11 +175,11 @@ window_functions:
     add edi, eax
     mov esi, [win_border_color]
     pop edx
-.loop:
+.loop5:
     mov [edi], edx
     add edi, eax
     dec ecx
-    jnz .loop
+    jnz .loop5
 
     mov [edi], esi
 
@@ -152,20 +198,79 @@ window_functions:
     add edi, eax
     pop edx
     dec ebx
-    jnz .loop
+    jnz .loop5
 
     mov esi, [win_border_color]
     movzx eax, byte [bpp]
     mov ecx, [win_width]
-.loop2:
+.loop6:
     mov [edi], esi
     add edi, eax
     dec ecx
-    jnz .loop2
+    jnz .loop6
     popa
+    mov ax, [window_id]
     iret
 
 .rm_window:
     pusha
+    mov eax, [edi+8]
+    mov [win_width], eax
+    mov eax, [edi+12]
+    mov [win_height], eax
+    mov eax, [edi+24]
+    mov [win_x], eax
+    mov eax, [edi+28]
+    mov [win_y], eax
+
+    movzx eax, bx
+    sub eax, 1
+    mov edi, windows_list
+    add edi, eax     ;add window ID
+    mov byte [edi], 0
+
+    movzx ecx, bx
+    mov eax, window_buffer
+    mul ecx
+    mov esi, eax
+
+    mov edi, [frame_buffer]
+    mov eax, [win_x]
+    sub eax, 1
+    movzx ebx, byte [bpp]
+    mul ebx
+    add edi, eax
+
+    mov eax, [win_y]
+    sub eax, 20
+    mov ebx, [pitch]
+    mul ebx
+    add edi, eax
+
+    mov ebx, [win_height]
+    add ebx, 21         ;header + last row
+
+    mov ecx, [win_width]
+    add ecx, 2
+    movzx eax, byte [bpp]
+    mul ecx
+    mov ecx, eax
+
+.rm_loop:
+    rep movsb
+
+    add edi, [pitch]
+    mov ecx, [win_width]
+    add ecx, 2
+    movzx eax, byte [bpp]
+    mul ecx
+    sub edi, eax
+    mov ecx, eax
+
+    dec ebx
+    jnz .rm_loop
     popa
     iret
+
+
+window_buffer   equ     0x200000
