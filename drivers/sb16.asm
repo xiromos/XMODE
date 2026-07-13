21cf533701107a11fb02ec7dd0bc782c1431e612
Technodon <subwayheimerwender@icloud.com>
2026-07-13 17:40:54 +0300

    new functions for cancelling, pausing and resuming play of WAV files

--- drivers/sb16.asm
+++ drivers/sb16.asm
@@ -33,6 +33,9 @@ init:
     mov edx, kernel_packet
     mov dword [edx], play_wav_file
     mov dword [edx+8], irq_handler
+    mov dword [edx+12], pause_play
+    mov dword [edx+16], resume_play
+    mov dword [edx+20], stop_play
     ret
 
 
@@ -265,6 +268,118 @@ dsp_write:
     pop edx
     ret
 
+; #### PAUSE / RESUME / STOP FUNCTIONS ####
+pause_play:
+    cmp word [bits8], 0
+    je .error
+    
+    cmp word [bits8], 8
+    je .pause8bit
+
+    mov al, 0xd5
+    call dsp_write
+    clc
+    ret
+.pause8bit:
+    mov al, 0xd0
+    call dsp_write
+    clc
+    ret
+.error:
+    stc
+    ret
+resume_play:
+    cmp word [bits8], 0
+    je pause_play.error
+
+    cmp word [bits8], 8
+    je .resume8bit
+
+    mov al, 0xd6
+    call dsp_write
+    clc
+    ret
+.resume8bit:
+    mov al, 0xd4
+    call dsp_write
+    clc
+    ret
+stop_play:
+    cmp byte [active], 0
+    je pause_play.error
+
+    cmp word [bits8], 8
+    je .stop8bit
+
+    ;mask channel
+    mov dx, 0xd4
+    mov al, 0x05
+    out dx, al
+
+    ;stop playing
+    mov al, 0xd5
+    call dsp_write
+
+    call .reset_dsp
+
+    mov dx, 0xd8
+    xor al, al
+    out dx, al  ;reset flip-flop
+
+    mov dx, 0xd4
+    mov al, 0x01
+    out dx, al
+
+    mov byte [active], 0
+    clc
+    ret
+.stop8bit:
+    ;mask channel
+    mov dx, 0x0a
+    mov al, 0x05
+    out dx, al
+    
+    mov al, 0xd0
+    call dsp_write
+
+    call .reset_dsp
+
+    mov dx, 0x0c
+    xor al, al
+    out dx, al
+
+    mov dx, 0x0a
+    mov al, 0x01
+    out dx, al
+
+    mov byte [active], 0
+    clc
+    ret
+.reset_dsp:
+    mov dx, 0x226
+    mov al, 1
+    out dx, al
+
+    mov dx, 0x80
+    in al, dx
+    in al, dx
+    in al, dx
+    in al, dx
+
+    mov dx, 0x226
+    xor al, al
+    out dx, al
+
+    mov dx, 0x22e
+.wait_dsp:
+    in al, dx
+    test al, (1 << 7)
+    jz .wait_dsp
+
+    mov dx, 0x22a
+    in al, dx
+    ret
+; #### INTERRUPT HANDLER ####
 
 irq_handler:
     pusha
@@ -349,6 +464,7 @@ irq_handler:
 
     mov dword [offset], 0
     mov byte [active], 0
+    mov word [bits8], 0
     popa
     iret
 
@@ -361,6 +477,9 @@ kernel_packet:
     resd 1        ;play WAV audio file
     resd 1
     resd 1        ;IRQ handler
+    resd 1        ;pause play
+    resd 1        ;resume play
+    resd 1        ;stop play
 mode: resb 1
 size: resd 1
 addr: resd 1
