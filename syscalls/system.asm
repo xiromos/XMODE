f143f6c8f799cdb4e9172a5b635ee5fc69e7f445
Technodon <subwayheimerwender@icloud.com>
2026-08-10 18:35:45 +0300

    network API: supports DNS and ICMP

--- syscalls/system.asm
+++ syscalls/system.asm
@@ -26,7 +26,13 @@
 ;AH = 0x21: network functions
 ;   AL = 0x01: get network stats
 ;       Input: EDI = pointer to 128B buffer
-;       Output: filled buffer
+;       EDI+0: device IPv4 address
+;       EDI+4: DNS server IPv4 address
+;       EDI+8: router IPv4 address
+;       EDI+20: number of successfully transmitted packets
+;       EDI+24: number of packets, failed to transmit
+;       EDI+28: number of successfully received packets
+;       EDO+32: number of packets, failed to receive
 ;   AL = 0x02: open socket
 ;       Input: BH = net_interface (0 = IPv4, 1 = IPv6), BL = protocol (0x01 ICMP, 0x02 TCP, 0x03 UDP), DX = port number (if zero then let the OS decide) ESI = buffer address for packets
 ;       Output: socket number in CX or CF if port number is already used
@@ -36,7 +42,19 @@
 ;       Input: CX = socket number
 ;       If returns, that means a packet was received. The program itself has to check if its the right packet.
 ;   AL = 0x05: close socket
-;       Input: CL = socket number
+;       Input: CX = socket number
+;   AL = 0x07: resolve domain name (like github.com) into IPv4 address
+;       Input: CX = socket number, EDI = pointer to null-terminated ASCII string of domain name
+;       Output: EDX = IPv4 address (big endian), EAX = Time to live
+;       Socket has to be UDP protocol
+;AH = 0x22: timer functions
+;   AL = 0x01: sleep X ms
+;       Input: EBX = number of miliseconds to sleep
+;   AL = 0x02: set counter
+;       Input: EBX = pointer to 4 Byte field which is incremented by 1 every 1ms, EDX = time in MS after which the timer expires and gets deleted (e.g. if EDX = 500, then the timer gets deleted after 500ms)
+;   AL = 0x03: get current time
+;       Output: AL = seconds (0 - 59), AH = minutes (0 - 59), BL = hour (0 - 23), BH = day (1 (monday) - 7 (sunday)), CL = day of month (1 - 31), CH = month (1 - 12), DL = year (0 - 99), DH = century (20)
+
 program_sys_handler:
     cmp ah, 0x00
     je general_system
@@ -70,6 +88,8 @@ program_sys_handler:
     je play_wav_file
     cmp ah, 0x21
     je network_functions
+    cmp ah, 0x22
+    je timer_functions
 
     or dword [esp+8], 1
     iret
@@ -365,7 +385,7 @@ start_task_fg2:
     or dword [esp+8], 1
     iret
 kill_task:
-    ;ESI = Taskname or EAX = PID
+    ;ESI = Taskname or EBX = PID
     ;If Taskname or PID is set, the other register should be zero
     pusha
     cmp ebx, 0
@@ -552,6 +572,16 @@ alloc_heap:
     dec ebp
     jnz .loop
 
+    movzx ebx, word [current_task]
+    cmp ebx, 1
+    je .critical_error
+
+    mov ah, 0x04
+    int 0x35
+
+    ;create window with error message: low memory
+    ;...
+
     popa
     or dword [esp+8], 1
     iret
@@ -578,7 +608,14 @@ alloc_heap:
     and dword [esp+8], 0xfffffffe
     iret
 .tmp: dd 0
-
+.error_msg: db 'Low Memory. Shell Tried To Search Free Memory But Failed.', 0x0a
+            db 'Try To Install More RAM. If You Installed Enough RAM, Maybe There Is A Memory Leak', 0 
+.critical_error:
+    mov esi, .error_msg
+    call rsod
+    ;call reboot
+    cli
+    hlt
 ;#################################################################
 ;################### FREE HEAP MEMORY ############################
 ;#################################################################
@@ -775,6 +812,13 @@ start_task_bg:
     ;ESI = Task Name
     ;EDI = filename
     ;EBX = pointer to directory
+
+    ;0xffffffff = no argument
+    ;all arguments have to be null-terminated
+    ;ECX = pointer to first argument
+    ;EDX = pointer to second argument
+    ;EBP = pointer to other arguments (which are stored in a structure for example)
+
     cli
     pusha
     cmp esi, 0
@@ -783,6 +827,10 @@ start_task_bg:
     je .error
 
     mov [.task_state], ah
+
+    mov [.first_arg], ecx
+    mov [.second_arg], edx
+    mov [.other_args], ebp
 
     push esi
     push ebx
@@ -865,13 +913,18 @@ start_task_bg:
     mov esp, eax
     mov eax, edx
 
-    ; mov eax, program_addr_off
-    ; imul eax, ecx
-    ; add eax, program_addr
+    ;push pointer to arguments
+    ;a value of 0xffffffff as pointer means, that there is no argument, the program should check for that
 
-    ;push arguments
-    mov esi, read_buffer2
-    mov edi, [argument2]
+    ;pop esi    ;pointer to 1. argument
+    ;pop edi    ;pointer to 2. argument
+    ;pop eax    ;pointer to other arguments
+
+    mov esi, [.other_args]
+    push esi
+
+    mov esi, [.first_arg]
+    mov edi, [.second_arg]
     push edi
     push esi
 
@@ -931,7 +984,9 @@ start_task_bg:
 .program_start: dd 0
 .size: dd 0
 .task_state: db 0       ;foreground / background
-
+.first_arg: dd 0
+.second_arg: dd 0
+.other_args: dd 0
 
 play_wav_file:
     ;EDI = address of file
@@ -1024,6 +1079,8 @@ network_functions:
 
     cmp byte [net_active], 1
     jne .no_network
+    cmp byte [net_stack_loaded], 1
+    jne .no_network
 
     cmp al, 0x01
     je .get_information
@@ -1035,18 +1092,25 @@ network_functions:
     je .wait_packet
     cmp al, 0x05
     je .close_socket
+    cmp al, 0x07
+    je .resolve_dns
 
     popa
     or dword [esp+8], 1
     iret
 
 .get_information:
+    mov esi, NET_INTERFACE
+    mov edx, packet_stats
+    mov ebx, [edx]
+    mov ebx, [ebx]
+    mov [edi+20], ebx
     jmp .done
 .open_socket:
     cmp bl, 0x03
     ja .error
     cmp bh, 0
-    jne .error      ;IPv6 yet no supported
+    jne .error      ;IPv6 yet not supported
 
     mov edi, socket_list
     xor ecx, ecx
@@ -1072,8 +1136,8 @@ network_functions:
 
     mov ebp, ecx
 
-    cmp bl, 0x02
-    jbe .skip_port  ;ICMP and ARP doesnt use ports
+    ; cmp bl, 0x01
+    ; je .skip_port  ;ICMP doesnt use ports
 
     cmp dx, 0
     jne .custom_port
@@ -1089,7 +1153,7 @@ network_functions:
     dec ecx
     jnz .loop2
     
-    mov edi, edx
+    mov edi, ebp
     imul edi, SOCKET_ENTRY_SIZE
     add edi, socket_list
     mov [edi+4], ax
@@ -1151,6 +1215,8 @@ network_functions:
     je .error
     cmp ecx, 0
     je .error
+    cmp ecx, 1500
+    ja .error
 
     movzx edi, bp
     imul edi, SOCKET_ENTRY_SIZE
@@ -1166,6 +1232,11 @@ network_functions:
 
     jmp .error
 .send_icmp:
+    mov ax, [edi+4]
+    mov edi, NET_INTERFACE
+    call dword [edi+32]
+    jc .no_network
+
     jmp .done
 .send_tcp:
     jmp .done
@@ -1180,6 +1251,7 @@ network_functions:
     mov ax, [edi+4]
     mov edi, NET_INTERFACE
     call dword [edi+20]     ;add_udp_header()
+    jc .no_network
 
     jmp .done
 
@@ -1209,12 +1281,252 @@ network_functions:
     mov edi, eax
     iret
 
+.resolve_dns:
+    push edi
+    push ecx
+    imul ecx, SOCKET_ENTRY_SIZE
+    mov edi, socket_list
+    add edi, ecx
+    cmp byte [edi+2], 0x03      ;check if its an UDP socket
+    pop ecx
+    pop edi
+    jne .error
+
+    mov esi, edi
+    call string_lowercase
+
+    mov edi, dns_packet
+    add edi, 13
+
+    push ecx
+
+    xor ebx, ebx
+    mov ecx, 20
+.loop_dns1:
+    lodsb
+
+    cmp al, 0x2e
+    je .done_dns1
+
+    stosb
+    inc ebx
+    dec ecx
+    jnz .loop_dns1
+
+    mov ebp, ecx
+    pop ecx
+
+    lodsb
+    cmp al, 0x2e
+    jne .error
+
+    push ecx
+    mov ecx, ebp
+
+.done_dns1:
+    mov edi, dns_packet
+    mov [edi+12], bl
+
+    add edi, 13     ;skip first 12 bytes + 1 byte length of domain
+    add edi, ebx    ;skip domain name
+    add edi, 1      ;skip length byte of domain ending
+    mov edx, ebx
+
+    xor ebx, ebx
+    mov ecx, 5
+.loop_dns2:
+    lodsb
+    cmp al, 0
+    je .done_dns2
+
+    stosb
+    inc ebx
+    dec ecx
+    jnz .loop_dns2
+
+    mov ebp, ecx
+    pop ecx
+
+    lodsb
+    cmp al, 0
+    jne .error
+
+    push ecx
+    mov ecx, ebp
+
+.done_dns2:
+    mov edi, dns_packet
+    add edi, 13     ;skip first 12 bytes + 1 byte length of domain name
+    add edi, edx    ;skip domain name
+    mov [edi], bl   ;set length of ending
+    inc edi         ;skip 'ending' byte
+    add edi, ebx    ;skip ending
+
+    xor al, al
+    stosb           ;set null-termination
+
+    mov word [edi], 0x0100
+    mov word [edi+2], 0x0100
+
+    pop ecx
+    push ecx
+
+    imul ecx, SOCKET_ENTRY_SIZE
+    mov edi, socket_list
+    add edi, ecx
+    pop ecx
+
+    mov ax, [current_task]
+    cmp ax, [edi]
+    jne .error
+
+    push ecx
+
+    mov eax, 53
+    shl eax, 16
+    mov ax, [edi+4]
+
+    mov ecx, 19
+    add ecx, ebx
+    add ecx, edx
+    mov esi, dns_packet
+
+    mov edi, NET_INTERFACE
+    mov edx, [edi+4]
+
+    call dword [edi+20]
+
+    pop ecx
+    ;[ TODO ]: set timeout for packets of 4 seconds
+.wait_packet_dns:
+    movzx edi, cx
+    imul edi, SOCKET_ENTRY_SIZE
+    add edi, socket_list
+    test byte [edi+3], (1 << 7)
+    jnz .done_dns
+
+    int 0x20
+    jmp .wait_packet_dns
+
+.done_dns:
+    and byte [edi+3], ~(1 << 7)
+    mov esi, [edi+6]
+
+    mov edi, dns_packet
+    add edi, 12
+
+    mov ecx, 32/4
+    xor eax, eax
+    rep stosd
+
+    add esi, 12
+    call check_dns_packet
+    jc .error_dns
+
+    mov [.tmp_ip], edx
+    mov [.tmp_ttl], eax
+
+    popa
+    mov edx, [.tmp_ip]
+    mov eax, [.tmp_ttl]
+    and dword [esp+8], 0xfffffffe
+    iret
+
+.error_dns:
+    cmp ah, 0xff
+    je .wait_packet_dns
+
+    popa
+    or dword [esp+8], 1
+    iret
+
+.tmp_ttl: dd 0
+.tmp_ip: dd 0
+check_dns_packet:
+    ;ESI = pointer to packet
+    ;Output: EDX = IPv4 address, EAX = TTL
+    cmp word [esi], 0x7777
+    jne .error2
+
+    mov ax, [esi+6]
+    xchg al, ah     ;swap bytes to get big endian
+    cmp ax, 0
+    je .error
+
+    mov edi, esi
+    add edi, 12
+    ;skip domain name
+    movzx eax, byte [edi]
+    inc edi
+    add edi, eax
+    movzx eax, byte [edi]
+    inc edi
+    add edi, eax
+    inc edi         ;skip null byte
+    add edi, 4      ;skip QTYPE and QCLASS
+    mov al, [edi]
+
+    add edi, 2
+
+    test al, (1 << 7)   ;test if there is the domain name again or just a pointer
+    jnz .skip           ;if its just a pointer then skip
+
+    sub edi, 2
+    movzx eax, byte [edi]
+    inc edi
+    add edi, eax
+    movzx eax, byte [edi]
+    inc edi
+    add edi, eax
+.skip:
+    cmp word [edi], 0x0100
+    jne .error  ;no IPv4
+
+    mov eax, [edi+4]     ;get TTL
+    call swap
+
+    add edi, 8
+
+    mov bx, [edi]
+    xchg bl, bh
+    cmp bx, 4
+    jne .error
+
+    mov edx, [edi+2]    ;IPv4 address in big endian
+
+    clc
+    ret
+.error:
+    ;error with DNS packet
+    xor ah, ah
+    stc
+    ret
+.error2:
+    ;it wasnt a DNS packet, wait longer
+    mov ah, 0xff
+    stc
+    ret
+
+swap:
+    ;swap bytes in EAX
+    push ebx
+    xor ebx, ebx
+    mov bx, ax
+    xchg bh, bl
+    shl ebx, 16
+    shr eax, 16
+    mov bx, ax
+    xchg bh, bl
+    mov eax, ebx
+    pop ebx
+    ret
+
 application_packet:
     ;ESI = pointer to packet
-    ;ECX = size
-    ;AX = destination port (port of program, which waits for the packet) (big endian)
-    ;BX = source port (from sender) (big endian)
-    ;EDX = source IPv4 address (from sender) (big endian)
+    ;ECX = size ## (if bit 15 is set, that means its an ICMP packet)
+    ;AX = destination port ## (port of program, which waits for the packet) ## (big endian) ## (if its an ICMP packet, then this is the ID (in little endian))
+    ;BX = source port ## (from sender) ## (big endian)
+    ;EDX = source IPv4 address ## (from sender) ## (big endian)
     ;copy packet into program packet buffer
     push ecx
     mov edi, socket_list
@@ -1274,3 +1586,375 @@ MAX_SOCKETS         equ 6
 ;     dw source_port    ;port, from where the packet came (big endian)
 ;     dw payload_length ;length of user data
 ;     dd packet_next    ;pointer to next packet if packets are fragmented (not implemented yet)
+
+dns_packet:
+    dw 0x7777   ;transaction ID
+    dw 0x0001   ;flags, big endian: 0x0100
+    dw 0x0100   ;questions count
+    times 3 dw 0
+
+    db 0        ;length of domain name (max. 20 bytes)
+    times 20 db 0
+    db 0        ;length of end (max. 5 bytes)
+    times 5 db 0
+    db 0        ;end of domain
+
+    dw 0x0100   ;big endian: 0x0001 (ask for IPv4 address)
+    dw 0x0100
+
+
+
+
+timer_functions:
+    cli
+    pusha
+    cmp al, 0x01
+    je .sleep
+    cmp al, 0x02
+    je .set_counter
+    cmp al, 0x03
+    je .get_time
+
+.error:
+    popa
+    or dword [esp+8], 1
+    iret
+
+; #### SLEEP ####
+.sleep:
+    mov edi, [sleep_timers_list]
+    mov ecx, [max_sleep_timers]
+.loop_sleep:
+    cmp word [edi], 0xff        ;deleted entry
+    je .found_free
+    cmp word [edi], 0
+    je .found_free
+
+    add edi, 8
+    dec ecx
+    jnz .loop_sleep
+    jmp .error
+
+.found_free:
+    mov ax, [current_task]
+    mov [edi], ax
+    mov eax, ebx
+    add eax, dword [system_tick]
+    mov [edi+4], eax
+
+    movzx edi, word [current_task]
+    imul edi, TASK_SIZE
+    add edi, tasks_esp
+
+    mov dword [edi+11], 0x0000b100  ;sleeping
+    sti
+    int 0x20
+    jmp .done
+
+; #### COUNTERS ####
+.set_counter:
+    mov ecx, [max_counters]
+    mov edi, [counters_list]
+.set_loop:
+    cmp dword [edi], 0xff
+    je .found_free_cntr
+    cmp dword [edi], 0
+    je .found_free_cntr
+
+    add edi, 8
+    dec ecx
+    jnz .set_loop
+    jmp .error
+
+.found_free_cntr:
+    mov [edi], ebx
+    mov eax, edx
+    add eax, dword [system_tick]
+    mov [edi+4], eax
+    jmp .done
+
+; #### TIME ####
+.get_time:
+    xor bp, bp
+
+    mov dx, 0x70
+    mov al, 0x0b
+    out dx, al
+
+    mov dx, 0x71
+    in al, dx
+
+    test al, (1 << 2)
+    jnz .binary_mode
+
+    mov bp, 1
+
+    mov dx, 0x70
+    xor al, al
+    out dx, al
+
+    mov dx, 0x71
+    in al, dx   ;get seconds
+    call bcd_convert_byte
+
+    mov bh, al
+
+    mov dx, 0x70
+    mov al, 0x02
+    out dx, al
+
+    mov dx, 0x71
+    in al, dx   ;get minutes
+    call bcd_convert_byte
+
+    mov ah, al
+    mov al, bh
+
+    push ax     ;push seconds (AL) and minutes (AH) to stack
+    jmp .hour_format
+
+.binary_mode:
+    mov dx, 0x70
+    xor al, al
+    out dx, al
+
+    mov dx, 0x71
+    in al, dx   ;get seconds
+    mov ah, al
+
+    mov dx, 0x70
+    mov al, 0x02
+    out dx, al
+
+    mov dx, 0x71
+    in al, dx   ;get minutes
+
+    xchg al, ah
+    
+    push ax     ;push seconds (AL) and minutes (AH) to stack
+
+.hour_format:
+
+    mov dx, 0x70
+    mov al, 0x0b
+    out dx, al
+
+    mov dx, 0x71
+    in al, dx
+
+    test al, (1 << 1)
+    jnz .24hour_format
+
+    mov dx, 0x70
+    mov al, 0x04
+    out dx, al
+
+    mov dx, 0x71
+    in al, dx   ;get hour (0 - 23)
+    mov bl, al
+
+    test bl, (1 << 7)
+    jz .get_month
+
+    and bl, ~(1 << 7)
+    add bl, 12
+
+    cmp bl, 24
+    jne .get_month
+
+    xor bl, bl
+    jmp .get_month
+.24hour_format:
+    mov dx, 0x70
+    mov al, 0x04
+    out dx, al
+
+    mov dx, 0x71
+    in al, dx   ;get hour (0 - 23)
+    mov bl, al
+
+.get_month:
+    push bx
+
+    mov dx, 0x70
+    mov al, 0x07
+    out dx, al
+
+    mov dx, 0x71
+    in al, dx
+
+    cmp bp, 1
+    jne .not_bcd1
+
+    call bcd_convert_byte
+.not_bcd1:
+    mov cl, al
+
+    mov dx, 0x70
+    mov al, 0x08
+    out dx, al
+
+    mov dx, 0x71
+    in al, dx
+
+    cmp bp, 1
+    jne .not_bcd2
+
+    call bcd_convert_byte
+.not_bcd2:
+    mov ch, al
+
+    push cx
+
+    mov dx, 0x70
+    mov al, 0x09
+    out dx, al
+
+    mov dx, 0x71
+    in al, dx
+
+    cmp bp, 1
+    jne .not_bcd
+
+    call bcd_convert_byte
+.not_bcd:
+    mov dl, al
+
+    mov dh, 20      ;century
+    cmp al, 85
+    jb .skip
+
+    mov dh, 19
+.skip:
+
+    pop cx
+    pop bx
+
+    push bx
+    push cx
+
+    mov bl, cl
+    mov bh, ch
+    movzx ecx, dh
+    imul ecx, 100   ;century * 100
+    movzx esi, dl
+    add ecx, esi
+
+    call get_weekday
+
+    pop cx
+    pop bx
+    mov bh, al
+    pop ax
+    cli
+    hlt
+.done:
+    popa
+    and dword [esp+8], 0xfffffffe
+    iret
+
+check_timers:
+    ;check first sleep timers
+    mov edi, [sleep_timers_list]
+    mov ecx, [max_sleep_timers]
+.loop:
+    cmp word [edi], 0xff
+    je .skip
+    cmp word [edi], 0
+    je .done_sleep
+
+    mov eax, [system_tick]
+    cmp eax, [edi+4]
+    jb .skip
+
+    movzx esi, word [edi]
+    imul esi, TASK_SIZE
+    add esi, tasks_esp
+
+    mov dword [esi+11], 0
+    mov word [edi], 0xff
+    mov dword [edi+4], 0
+.skip:
+    add edi, 8
+    dec ecx
+    jnz .loop
+
+.done_sleep:
+    mov edi, [counters_list]
+    mov ecx, [max_counters]
+.loop2:
+    cmp dword [edi], 0xff
+    je .skip2
+    cmp dword [edi], 0
+    je .done
+
+    mov esi, [edi]
+    inc dword [esi]
+
+    mov eax, [system_tick]
+    cmp eax, [edi+4]
+    jb .skip2
+
+    mov dword [edi], 0xff
+    mov dword [edi+4], 0
+.skip2:
+    add edi, 8
+    dec ecx
+    jnz .loop2
+
+.done:
+    ret
+
+
+get_weekday:
+    ;BL = day (1-31)
+    ;BH = month (1-12)
+    ;CX = year (for example 2027)
+
+    ;Output: AL = weekday (0 = sunday, 1 = monday...)
+    push ebx
+    push ecx
+    push edx
+    push esi
+    push edi
+
+    movzx eax, bh
+    cmp al, 3
+    jge .not_first2
+    dec cx
+.not_first2:
+    movzx edi, cx
+    mov eax, edi
+    shr eax, 2
+    add eax, edi
+    mov esi, eax
+
+    mov eax, edi
+    xor edx, edx
+    mov ecx, 100
+    div ecx
+    add esi, eax
+
+    movzx eax, bh
+    dec eax
+    mov edx, t_table
+    movzx edx, byte [edx+eax]
+    add esi, edx
+
+    movzx eax, bl
+    add esi, eax
+
+    mov eax, esi
+    xor edx, edx
+    mov ecx, 7
+    div ecx
+
+    mov al, dl
+
+    pop edi
+    pop esi
+    pop edx
+    pop ecx
+    pop ebx
+    ret
+t_table: db 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4
